196e9fa7f34475a8a9951c0e9e3876a1d80f5f5d
KShervington <kyle.shervington@gmail.com>
2024-03-29 11:32:33 -0400

    moved frontend folder to src folder

--- src/frontend/src/App.test.js
+++ src/frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const placeholder = screen.getByText(/placeholder/i);
-  expect(placeholder).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
