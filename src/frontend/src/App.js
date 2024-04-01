6ed9445586e8f80bb0ea27f0ed230b02dff4d05d
KShervington <kyle.shervington@gmail.com>
2024-03-31 23:31:55 -0400

    tailwindcss setup

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,13 @@
-
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Placeholder Text</h1>
+    <div className='App'>
+      <div id='pageBody' className='bg-gradient-to-b from-slate-900 to-slate-950 h-screen w-full flex justify-center items-center font-sans'>
+        <div id='mainContent' className='flex flex-col flex-wrap justify-center items-center w-10/12'>
+          <h1 className='text-slate-400 text-9xl'>Placeholder Text</h1>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Helvetica', 'ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+    },
+  },
+  plugins: [],
+}
