8778397c2bf4b3a12b631e42995da38a33ffdc80
Alexandra Arriola <aalexarr.io@gmail.com>
2024-04-02 15:47:40 -0400

    Added template rendering for potentially loading up dashboard home page

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Members API Route
-
-@app.route("/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
