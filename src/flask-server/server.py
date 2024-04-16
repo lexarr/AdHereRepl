90261d3ea6ae8a39a905a95f6d9a2fd5529e5b65
Dhruvi Shah <dhruvishah549@gmail.com>
2024-04-16 11:28:08 -0400

    Removed unnecessary print statements

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -10,16 +10,13 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 def url_check():
     url = request.args.get('url')
     try:
-        # The following logic was adapted from https://www.geeksforgeeks.org/test-the-given-page-is-found-or-not-on-the-server-using-python/#
+        # The following logic was adapted from https://www.geeksforgeeks.org/test-the-given-page-is-found-or-not-on-the-server-using-python/
         response = requests.head('http://www.' + url)
         if response.status_code != 200:
-            print('status code = ', response.status_code)
             return {'does_url_exist': False}
         else:
-            print('here1')
             return {'does_url_exist': True}
     except requests.ConnectionError:
-        print('here2')
         return {'does_url_exist': False}
 
 if __name__ == '__main__':
