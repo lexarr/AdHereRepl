d58f21ca860ee72ea6c46cad7326a85eaa167f85
KShervington <kyle.shervington@gmail.com>
2024-04-17 01:29:59 -0400

    removed MobX-related code and documentation

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,27 +1,23 @@
 import "./App.css";
-import { MainStoreProvider } from "./MainStoreContext";
-import MainStore from "./MainStore";
 import Header from "./components/Header";
 import ViolatingSitesTable from "./components/ViolatingSitesTable";
 
 function App(): JSX.Element {
   return (
-    <MainStoreProvider store={MainStore}>
-      <div
-        id="pageContainer"
-        className="bg-gradient-to-b dark:from-slate-900 dark:to-slate-950 from-slate-100 to-slate-200 pb-28 lg:pb-0"
-      >
-        <div className="App">
-          <Header />
-          <div
-            id="pageBody"
-            className="flex flex-col flex-wrap justify-center items-center h-screen font-sans"
-          >
-            <ViolatingSitesTable />
-          </div>
+    <div
+      id="pageContainer"
+      className="bg-gradient-to-b dark:from-slate-900 dark:to-slate-950 from-slate-100 to-slate-200 pb-28 lg:pb-0"
+    >
+      <div className="App">
+        <Header />
+        <div
+          id="pageBody"
+          className="flex flex-col flex-wrap justify-center items-center h-screen font-sans"
+        >
+          <ViolatingSitesTable />
         </div>
       </div>
-    </MainStoreProvider>
+    </div>
   );
 }
 
